527c2ff69cea2f3fd6a5231639c0ddef112ee925
Alex <alex.kojokari@gmail.com>
2023-06-25 10:03:35 +0300

    Completed Task_002

---
+++ Homework_004/Task_002.py
@@ -0,0 +1,15 @@
+# Напишите функцию принимающую на вход только ключевые 
+# параметры и возвращающую словарь, где ключ — значение 
+# переданного аргумента, а значение — имя аргумента. 
+# Если ключ не хешируем, используйте его строковое представление.
+
+def create_dict(**kwargs):
+    result_dict = {}
+    for key, value in kwargs.items():
+        if not isinstance(key, (int, float, str, bool)):
+            key = str(key)
+        result_dict[value] = key
+    return result_dict
+
+result = create_dict(a=word, b='hello', c=True, d=3.14)
+print(result)
